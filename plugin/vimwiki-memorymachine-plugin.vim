d3363b9ca2035c24d13b386bededadd830259afe
juaneduardoflores <juanedflores@gmail.com>
2020-09-20 14:41:59 -0500

    added global variables

--- plugin/vimwiki-memorymachine-plugin.vim
+++ plugin/vimwiki-memorymachine-plugin.vim
@@ -1,8 +1,2 @@
-
-" The VimL/VimScript code is included in this sample plugin to demonstrate the
-" two different approaches but it is not required you use VimL. Feel free to
-" delete this code and proceed without it.
-
-function DoItVimL()
-    echo "hello from DoItVimL"
-endfunction
+let g:MemMachineEnable = 0
+let g:MemMachineIndex = "/Users/juaneduardoflores/wiki/Notes/notes_index.html.md"
--- rplugin/python3/vimwiki-memorymachine-plugin.py
+++ rplugin/python3/vimwiki-memorymachine-plugin.py
@@ -1,4 +1,7 @@
+# -*- coding: UTF-8 -*-
 import pynvim
+import os
+import re
 
 
 @pynvim.plugin
@@ -7,22 +10,48 @@ class Main(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.toggle = False
+        self.lines = []
 
     @pynvim.function('GetFileContents', sync=True)
     def getFileContents(self, args):
-        for i in range(len(self.nvim.current.buffer)):
-            self.nvim.current.buffer[i] += str(
-                len(self.nvim.current.buffer[i]))
+        buf = self.nvim.current.buffer
+        pattern = re.compile(r'^\s*\s\*\s\[.+\]\((?P<dir>.+?)\)')
+        dir_path = os.path.dirname(buf.name)
+        directories = os.listdir(dir_path)
+        appendedpattern = re.compile(r'\s\[\s\d+\s\]$')
+        totalappendedpattern = re.compile(r'.*\s\[\s\d+\s\]$')
 
-    @pynvim.command('MemMachToggle', nargs=0, range=None)
+        if (self.toggle):
+            for i in range(len(buf)):
+                line = buf[i]
+                m = pattern.match(line)
+                a = totalappendedpattern.match(line)
+
+                if (m and not a):
+                    match_dir = m.group('dir')
+                    match_dir_path = os.path.dirname(match_dir)
+                    if (match_dir_path in directories):
+                        match_dir_path_dirs = os.listdir(match_dir_path)
+                        buf[i] += ' ' + \
+                            '[ ' + str(len(match_dir_path_dirs)) + ' ]'
+        else:
+            for i in range(len(buf)):
+                line = buf[i]
+                buf[i] = re.sub(appendedpattern, '', line)
+
+    @ pynvim.command('MemMachineToggle', nargs=0, range=None)
     def testcommand(self):
         self.toggle = not self.toggle
-        if (self.toggle):
-            self.nvim.command('call GetFileContents()')
-        # TODO: Untoggle should remove all the added info
+        self.nvim.command('call GetFileContents()')
 
     @ pynvim.autocmd('BufEnter', pattern='*.md', eval=None, sync=False)
     def on_vimwikienter(self):
-        if (self.toggle == True):
-            # TODO: Check if file is a vimwiki filetype
+        is_enabled = self.nvim.eval('g:MemMachineEnable')
+        buf = self.nvim.current.buffer
+        MemIndex = self.nvim.eval('g:MemMachineIndex')
+        if (buf.name == MemIndex):
+            if (is_enabled):
+                self.toggle = True
+            else:
+                self.toggle = False
             self.nvim.command('call GetFileContents()')
