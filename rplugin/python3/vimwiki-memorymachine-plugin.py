b7f12743c08cde0def38ec5e9e399732f3efe89b
juaneduardoflores <juanedflores@gmail.com>
2020-09-20 17:35:03 -0500

    made info align vertically by char length of 60

--- rplugin/python3/vimwiki-memorymachine-plugin.py
+++ rplugin/python3/vimwiki-memorymachine-plugin.py
@@ -10,33 +10,66 @@ class Main(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.toggle = False
-        self.lines = []
 
     @pynvim.function('GetFileContents', sync=True)
     def getFileContents(self, args):
+        # get the current buffer
         buf = self.nvim.current.buffer
-        pattern = re.compile(r'^\s*\s\*\s\[.+\]\((?P<dir>.+?)\)')
-        dir_path = os.path.dirname(buf.name)
-        directories = os.listdir(dir_path)
-        appendedpattern = re.compile(r'\s\[\s\d+\s\]$')
-        totalappendedpattern = re.compile(r'.*\s\[\s\d+\s\]$')
+        # regex patterns
+        pattern = re.compile(r'^\s*\*\s\[(?P<cat>.+?)\]\((?P<dir>.+?)\)')
+        # appendedpattern = re.compile(r'\s*\[\s\d+\s\]$')
+        # appendedpattern = re.compile(r'\s*\[\s.+\d+\s\]$')
+        appendedpattern = re.compile(r'\s*\bfiles:\s\d+\b$')
+        # totalappendedpattern = re.compile(r'.*\s\[\s\d+\s\]$')
+        totalappendedpattern = re.compile(r'.*\s*\bfiles:\s\d+\b$')
+        # get the directory of the current buffer
+        cur_dir_path = os.path.dirname(buf.name)
+        # get the list of files/directories in the current directory
+        directories = os.listdir(cur_dir_path)
 
         if (self.toggle):
             for i in range(len(buf)):
+                # get line index i of buffer
                 line = buf[i]
+                lengthofline = len(line)
+                # check if line is in the format we are concerned with
                 m = pattern.match(line)
+                # check if line already has appended info
                 a = totalappendedpattern.match(line)
+                # find any backticks
+                backticks = len(re.findall(r'`.*?`', line)) * 2
 
+                # if line is format we are concerned with and has
+                # not been appended with info.
                 if (m and not a):
+                    # get the group in pattern that has the directory
                     match_dir = m.group('dir')
+                    # get the path of the directory in link
                     match_dir_path = os.path.dirname(match_dir)
+
+                    # if found directory in line matches a directory in current
+                    # buffer directory
                     if (match_dir_path in directories):
+                        # get the files/directories of found directory
                         match_dir_path_dirs = os.listdir(match_dir_path)
-                        buf[i] += ' ' + \
-                            '[ ' + str(len(match_dir_path_dirs)) + ' ]'
+
+                        # find how many spaces to append.
+                        # get the longest char line.
+                        # TODO: get amount of all hidden vimwiki chars (bold, italics)
+                        visiblelength = lengthofline - \
+                            (len(match_dir) + 4) - backticks
+                        spaces = ""
+                        if (visiblelength < 60):
+                            spaces += " " * (60 - visiblelength)
+
+                        buf[i] += spaces + "files: " + \
+                            str(len(match_dir_path_dirs))
+
+        # if not toggled on, remove appended info if any
         else:
             for i in range(len(buf)):
                 line = buf[i]
+                # get the substring without appended info
                 buf[i] = re.sub(appendedpattern, '', line)
 
     @ pynvim.command('MemMachineToggle', nargs=0, range=None)
@@ -50,6 +83,7 @@ class Main(object):
         buf = self.nvim.current.buffer
         MemIndex = self.nvim.eval('g:MemMachineIndex')
         if (buf.name == MemIndex):
+            self.nvim.command('setlocal nowrap')
             if (is_enabled):
                 self.toggle = True
             else:
