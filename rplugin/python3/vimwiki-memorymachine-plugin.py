5c6c63468541b270678a7e77257bc7ce12e0e0e6
juaneduardoflores <juanedflores@gmail.com>
2020-09-21 14:43:30 -0500

    added highlighting

--- rplugin/python3/vimwiki-memorymachine-plugin.py
+++ rplugin/python3/vimwiki-memorymachine-plugin.py
@@ -10,71 +10,62 @@ class Main(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.toggle = False
+        # self.nvim.command("sy match memMachineGood '\vgood$'")
 
-    @pynvim.function('GetFileContents', sync=True)
+    @pynvim.function('GetFileContents', sync=True, allow_nested=False)
     def getFileContents(self, args):
         # get the current buffer
         buf = self.nvim.current.buffer
+        # pattern to see if file already has appended info
+        appendedpattern = re.compile(r'.*\s*\bfiles:\s\d+\b$')
+        onlyappendedpattern = re.compile(r'\s*\bfiles:\s\d+\b$')
         # regex patterns
-        pattern = re.compile(r'^\s*\*\s\[(?P<cat>.+?)\]\((?P<dir>.+?)\)')
-        # appendedpattern = re.compile(r'\s*\[\s\d+\s\]$')
-        # appendedpattern = re.compile(r'\s*\[\s.+\d+\s\]$')
-        appendedpattern = re.compile(r'\s*\bfiles:\s\d+\b$')
-        # totalappendedpattern = re.compile(r'.*\s\[\s\d+\s\]$')
-        totalappendedpattern = re.compile(r'.*\s*\bfiles:\s\d+\b$')
+        unappendedpattern = re.compile(
+            r'^\s*\*\s\[(?P<cat>.+?)\]\((?P<dir>.+?)\)')
+
         # get the directory of the current buffer
         cur_dir_path = os.path.dirname(buf.name)
         # get the list of files/directories in the current directory
         directories = os.listdir(cur_dir_path)
 
-        if (self.toggle):
-            for i in range(len(buf)):
-                # get line index i of buffer
-                line = buf[i]
-                lengthofline = len(line)
-                # check if line is in the format we are concerned with
-                m = pattern.match(line)
-                # check if line already has appended info
-                a = totalappendedpattern.match(line)
-                # find any backticks
-                backticks = len(re.findall(r'`.*?`', line)) * 2
+        # check if already appended
+        for i in range(len(buf)):
+            line = buf[i]
+            appendedmatch = appendedpattern.match(line)
+            unappendedmatch = unappendedpattern.match(line)
 
-                # if line is format we are concerned with and has
-                # not been appended with info.
-                if (m and not a):
-                    # get the group in pattern that has the directory
-                    match_dir = m.group('dir')
-                    # get the path of the directory in link
+            # get nvim highlight source
+            src_id = self.nvim.new_highlight_source()
+
+            if (self.toggle):
+                if (appendedmatch):
+                    lengthofunappended = len(
+                        re.sub(onlyappendedpattern, '', line))
+                    # highlight line
+                    buf.add_highlight(
+                        "Comment", i, lengthofunappended, -1, src_id)
+                elif (unappendedmatch and not appendedmatch):
+                    backticks = len(re.findall(r'`.*?`', line)) * 2
+                    match_dir = unappendedmatch.group('dir')
                     match_dir_path = os.path.dirname(match_dir)
-
-                    # if found directory in line matches a directory in current
-                    # buffer directory
                     if (match_dir_path in directories):
-                        # get the files/directories of found directory
                         match_dir_path_dirs = os.listdir(match_dir_path)
-
-                        # find how many spaces to append.
-                        # get the longest char line.
-                        # TODO:
+                        lengthofline = len(line)
                         visiblelength = lengthofline - \
                             (len(match_dir) + 4) - backticks
                         spaces = ""
                         if (visiblelength < 60):
                             spaces += " " * (60 - visiblelength)
-
-                        # append info
-                        # buf[i] += ' ' + \
-                        # '[ ' + str(len(match_dir_path_dirs)) + ' ]'
-
+                        # add file info
                         buf[i] += spaces + "files: " + \
-                            str(len(match_dir_path_dirs))
+                            str(len(match_dir_path_dirs)-1)
+                        # highlight line
+                        buf.update_highlights(
+                            src_id, [("Comment", i, lengthofline, -1)], clear=True, async_=False)
 
-        # if not toggled on, remove appended info if any
-        else:
-            for i in range(len(buf)):
-                line = buf[i]
-                # get the substring without appended info
-                buf[i] = re.sub(appendedpattern, '', line)
+            else:
+                if (appendedmatch):
+                    buf[i] = re.sub(onlyappendedpattern, '', line)
 
     @ pynvim.command('MemMachineToggle', nargs=0, range=None)
     def testcommand(self):
@@ -92,4 +83,5 @@ class Main(object):
                 self.toggle = True
             else:
                 self.toggle = False
-            self.nvim.command('call GetFileContents()')
+            if (buf.valid == True):
+                self.nvim.command('call GetFileContents()')
